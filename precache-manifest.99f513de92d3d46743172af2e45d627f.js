84232c7253dbbeb52dcd0283819c2ce3281eef6c
One Nights <one@Miras-MacBook-Pro.local>
2019-01-22 19:08:42 +0000

    Updates

---
+++ precache-manifest.99f513de92d3d46743172af2e45d627f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6da5f9042b498125c89",
+    "url": "/seasonal-cuisine/static/css/main.f1485233.chunk.css"
+  },
+  {
+    "revision": "e6da5f9042b498125c89",
+    "url": "/seasonal-cuisine/static/js/main.e6da5f90.chunk.js"
+  },
+  {
+    "revision": "64891293861cae912477",
+    "url": "/seasonal-cuisine/static/js/1.64891293.chunk.js"
+  },
+  {
+    "revision": "0784e9ce00cc1b9a6344",
+    "url": "/seasonal-cuisine/static/js/runtime~main.0784e9ce.js"
+  },
+  {
+    "revision": "9c3dc666a3d4971cbd342024c587ea39",
+    "url": "/seasonal-cuisine/static/media/chicken.9c3dc666.jpg"
+  },
+  {
+    "revision": "1fd31968719c79f60be7fc6c2b318c38",
+    "url": "/seasonal-cuisine/static/media/cookies.1fd31968.jpg"
+  },
+  {
+    "revision": "4cbd99d251efa4d2dd91d228437810a8",
+    "url": "/seasonal-cuisine/static/media/cookies2.4cbd99d2.jpg"
+  },
+  {
+    "revision": "bb5d028b0fed3945c5be90ee280852d5",
+    "url": "/seasonal-cuisine/static/media/meat.bb5d028b.jpg"
+  },
+  {
+    "revision": "db87f7696ec531d5ce39ec9aa416d070",
+    "url": "/seasonal-cuisine/static/media/meat2.db87f769.jpg"
+  },
+  {
+    "revision": "69e02093b55540027b72b93d8f27d29e",
+    "url": "/seasonal-cuisine/static/media/logoSC.69e02093.png"
+  },
+  {
+    "revision": "b8697c3321255f6dabe191c2c320a6a8",
+    "url": "/seasonal-cuisine/static/media/hero-img.b8697c33.jpg"
+  },
+  {
+    "revision": "c0bc42d39e52bb7e16154e77c403708f",
+    "url": "/seasonal-cuisine/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/seasonal-cuisine/precache-manifest.99f513de92d3d46743172af2e45d627f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/seasonal-cuisine/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
